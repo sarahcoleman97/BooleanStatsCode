537a87024781cc51952abdd5a8be046605cf9b7b
Sarah M. Coleman <sarahcol@utexas.edu>
2025-09-28 13:45:29 -0500

    finish figs3

--- figs3/power_analysis.R
+++ figs3/power_analysis.R
@@ -1,13 +1,12 @@
 setwd("~/Library/CloudStorage/Box-Box/BooleanStats/BooleanStatsCode/figs3")
 
-source("simulate_data.R") # allows to pass a variance input, fraction of the mean response.
-
 set.seed(2024) 
 
-library('lme4')
-library('effects')
-library('broom')
-library('multcomp')
+library(lme4)
+library(effects)
+library(broom)
+library(dplyr)
+library(multcomp)
 
 # Define logics
 and.logic <- c(0, 0, 0, 1)
@@ -30,74 +29,130 @@ logic_list <- list('AND' = and.logic,
                    'NIMPLY' = nimply.logic)
 
 # Define simulation conditions
-new.cond <- read.csv("simulation_conditions.csv",row.names=1,header=TRUE)
-conditions <- new.cond |> row.names()
-conditions_logics <- c(conditions[1:8], "OR") # specific to this case
+sim.means <- read.csv("simulation_conditions.csv",row.names=1,header=TRUE)
+conditions <- sim.means |> row.names()
 samples <- 2:30
-n.mc <- 10 # number of monte carlo simulations
 variance.values <- 1:10 * 0.1
-or.logic <- c(0, 1, 1, 1)
+n.mc <- 1e4
 
-conditions_idx <- 1:length(conditions)
-variance_idx <- 1:length(variance.values)
-samples_idx <- 1:length(samples)
-mc_idx <- 1:n.mc
+# Make a matrix of all the condtions I want to test
+colNames <- c('Condition', 'Variance', 'Sample.Size')
+combs <- expand.grid(conditions, variance.values, samples,
+                     KEEP.OUT.ATTRS = FALSE, stringsAsFactors = FALSE) |> 
+  rename_with(~colNames) |> 
+  mutate(Logic = if_else(Condition == 'OR1','OR',Condition)) |> 
+  as.matrix()
 
-result_top <- parallel::mclapply(conditions_idx, function(i){
-  condition <- conditions[i]
-  logic <- logic_list[[conditions_logics[[i]]]]
+# Turn into list and get sim means for each row, and logic
+entryNames <- c('Condition', 'Variance', 'Sample.Size','Logic')
+comb_list_pt1 <- split(combs, row(combs))
+
+# Define cores for parallel stuff 
+n_cores <- parallel::detectCores() - 2
+
+condition_list <- parallel::mclapply(comb_list_pt1, function(row){
   
-  result_n1 <- parallel::mclapply(variance_idx, function(m){
-    variance <- variance.values[m]
-    beta.store <- matrix(nrow=length(samples),ncol=n.mc) # init
-    p.value <- matrix(nrow=length(samples),ncol=n.mc) # init
-    singular.store <- matrix(nrow=length(samples),ncol=n.mc) # init
+  # Make each row a list, name it
+  row <- row |> as.list() 
+  names(row) <- entryNames 
+  
+  # Make sure classes are correct
+  row[['Variance']] <- row[['Variance']] |> as.numeric()
+  row[['Sample.Size']] <- row[['Sample.Size']] |> as.numeric()
+  
+  # Add additional components
+  Means <- sim.means[row[['Condition']],] # get the sim.means
+  Logic <- logic_list[[row[['Logic']]]]
+  row_final <- list('Row' = row, 'Means' = Means, 'Logic' = Logic)
+}, mc.cores = n_cores)
 
-    result_n2 <- parallel::mclapply(samples_idx, function(j){ 
-      n <- samples[j]
-      
-      result_n3 <- parallel::mclapply(mc_idx, function(k){ 
-        working.df <- simulate_data(new.cond[i,], 
-                                 n, 
-                                 logic, 
-                                 paste0('sim_data/',
-                                        condition),
-                                 variance = variance,
-                                 write.file = FALSE)
-        working.lmer <- lmer(Results ~ on + (1|group), data = working.df)
-        Singular <- isSingular(working.lmer)
+# Simulate data (doing mclapply inside a for list due to memory issues)
+
+pb <- txtProgressBar(min = 0, max = length(condition_list), style = 3)
+prog <- 0
+
+
+# Final loop (takes a while)
+for (ind_list in condition_list){ # doing this as a for loop to reduce overwhelm
+  
+  # Get conditions
+  condition <- ind_list[['Row']][['Condition']]
+  variance <- ind_list[['Row']][['Variance']] |> as.numeric()
+  sample.size <- ind_list[['Row']][['Sample.Size']] |> as.integer()
+  working.params.df <- ind_list[['Means']]
+  logic <- ind_list[['Logic']]
+  
+  # Making factors
+  group <- rep(c(1:4), each = sample.size) |> as.factor()
+  on <- rep(logic, each = sample.size)
+  
+  # Simulate data
+  df_list <- parallel::mclapply(1:n.mc, function(mc){ # for each mc
+  
+    # Simulating
+    var <- variance * (working.params.df |> unlist())
+    Results <- lapply(names(var), function(name){
+      sim <- rnorm(sample.size, working.params.df[[name]], var[[name]])
+    }) |> unlist()
+
+    # Construct df
+    working.df <- data.frame(Results, on, group) 
+  }) 
+  
+  # Stats
+  result_df <- parallel::mclapply(1:n.mc, function(mc){
     
-        if (Singular){ # refit without random effects
-          working.lmer <- lm(Results ~ on, data = working.df)
-          beta.estimate <- coef(working.lmer)[['on']]
-        } else {
-          beta.estimate <- coef(working.lmer)[['group']][['on']][1]
-        }
-        
-        answer <- summary(glht(working.lmer,linfct=c('on==0')))
-        beta.store[j,k] <- beta.estimate
-        p.value[j,k] <- answer$test$pvalues[1]
-        singular.store[j,k] <- Singular
-      })
-    })
+    # Get df
+    df <- df_list[[mc]]
     
-    # Write outputs
-    beta.store <- data.frame(beta.store, row.names = samples)
-    colnames(beta.store) <- mc_idx
-    beta.store <- t(beta.store)
-    write.csv(beta.store,paste0('beta_montecarlo/',condition,
-                                '_betas_var_',variance,'.csv'))
-      
-    p.value <- data.frame(p.value, row.names = samples)
-    colnames(p.value) <- mc_idx
-    p.value <- t(p.value)
-    write.csv(p.value,paste0('beta_montecarlo/',condition,
-                             '_pvals_var_',variance,'.csv'))
+    # Fit model, check for singularity
+    working.lmer <- lmer(Results ~ on + (1|group), data = df)
+    Singular <- isSingular(working.lmer)
     
-    singularities <- data.frame(singular.store, row.names = samples)
-    colnames(singularities) <- mc_idx
-    singularities <- t(singularities)
-    write.csv(singularities,paste0('beta_montecarlo/',condition,
-                                   '_singularities_var_',variance,'.csv'))
-  })
-})
+    if (Singular){ # refit without random effects
+      working.lmer <- lm(Results ~ on, data = df)
+      beta.estimate <- coef(working.lmer)[['on']]
+    } else {
+      beta.estimate <- coef(working.lmer)[['group']][['on']][1]
+    }
+    
+    # Do test
+    answer <- glht(working.lmer,linfct=c('on==0')) |> tidy()
+    p.val <- (answer |> pull(adj.p.value))[1]
+    
+    result <- data.frame('MonteCarlo' = mc,
+                         'Beta' = beta.estimate, 
+                         'p.value' = p.val,
+                         'singular' = Singular)
+  }, mc.cores = n_cores) |> bind_rows()
+  
+  write.csv(result_df, 
+            paste0('beta_montecarlo/',condition,
+                   '_var_',variance,
+                   '_n_',sample.size,
+                   '.csv'),
+            row.names = F)
+  
+  prog <- prog + 1
+  setTxtProgressBar(pb, prog)
+}
+
+# Pull out the select samples to make it easy to copy into Prism for plotting
+samples_select <- c(3, 5, 30)
+
+for (condition in conditions){
+  files <- paste0('beta_montecarlo/', condition, 
+                  '_var_0.1_n_',samples_select, '.csv')
+  
+  df <- lapply(samples_select, function(sample) {
+    f <- paste0('beta_montecarlo/', condition, 
+                '_var_0.1_n_',sample, '.csv') 
+    newColName <- paste0('Beta_n_', sample)
+    dat <- read.csv(f) |> 
+      dplyr::select(Beta) |> 
+      rename_with(~newColName)
+  }) |> bind_cols()
+  
+  write.csv(df, paste0('sim_data/', condition, 
+                       '_betas_', 'n_3_5_30.csv'))
+}
